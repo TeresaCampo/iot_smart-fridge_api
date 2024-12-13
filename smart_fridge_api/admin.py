4f89bdb110499df3f78d6e1926c8c0f5496be6a4
TeresaCampo <terry.campo.2002@gmail.com>
2024-12-13 01:34:28 +0100

    Admin page updated with Parameter table

--- smart_fridge_api/admin.py
+++ smart_fridge_api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product, Fridge
+from .models import Product, Fridge, Parameter
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Fridge)
+admin.site.register(Parameter)
 
 
