513afe796d57ee5868ef47e6d13fa0ae70811502
TeresaCampo <terry.campo.2002@gmail.com>
2025-01-31 15:26:12 +0100

    updated command populate_db

--- smart_fridge_api/management/commands/populate_db.py
+++ smart_fridge_api/management/commands/populate_db.py
@@ -37,6 +37,9 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS("Database initialized with default values"))
         
         #users associated to fridges 1,2,3 are always the same:
-        # fridge 1 -> Anna Bianchini, token: c5c90de5802459f2e9b267f7f63857ed9c1c6e7b
+        #fridge 1 -> Anna Bianchini, token: c5c90de5802459f2e9b267f7f63857ed9c1c6e7b
         #fridge 2 -> Andrea Rossi, token: 23dc9513ea976e202f306fd7d93c5093a066c5f0
         #fridge 3 -> Maria Verdi, token: 74d7cf3d048ba4f92ef2eb6352e7326e0996c56e
+
+        #superuser
+        # campoteresa.2002@gmail.com -> 62aa1bd2271eedd587232a3259f262fa5b578d88
